9e2762317d71a9556be2cc8f6bde3c7af041ffbc
Albertus Angga Raharja <hello@albertus.dev>
2023-01-21 18:48:39 +0700

    Initial SvelteKit SSG repo setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div>
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation!
+  </p>
+</div>
+
+<style global>
+  div {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
