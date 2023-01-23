44357b7b937ac0d329edb0b860e03320053120eb
Albertus Angga Raharja <hello@albertus.dev>
2023-01-23 18:11:29 +0700

    Setup primary font family and basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,39 @@
+<script>
+  import "../app.css";
+</script>
+
 <div>
-  <h1>Welcome to SvelteKit</h1>
-  <p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation!
-  </p>
+  <div>
+    <h1>Albertus Angga Raharja</h1>
+    <p>
+      Visit <a href="https://albertus.dev">albertus.dev</a> to see the old crappy
+      website!
+    </p>
+  </div>
+  <div />
 </div>
 
-<style global>
-  div {
-    width: 100vw;
+<style>
+  div:first-of-type {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    place-items: center;
+    width: 100%;
     height: 100vh;
+  }
+
+  div:first-of-type > div:first-child {
+    padding: 1rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+  }
+
+  div:first-of-type > div:last-child {
+    padding: 1rem;
+    background-color: #f5f5f5;
+    width: 100%;
+    height: 100%;
   }
 </style>
