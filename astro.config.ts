353486bb4cf09ee9a7aa1f120be89372c1ff241d
Albertus Angga Raharja <hello@albertus.dev>
2023-07-14 15:27:09 +0800

    chore: migrate to Astro markdoc integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import markdoc from "@astrojs/markdoc";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [markdoc()]
 });
